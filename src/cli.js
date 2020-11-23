bff62cb2a4be3fb667b76ecabd905cea83808b57
Alonso Zamorano <alonso.zamorano97@gmail.com>
2020-11-23 20:36:01 -0300

    Update

--- src/cli.js
+++ src/cli.js
@@ -5,31 +5,30 @@ const bookmakerBuild = require('./bookmakerBuild')
 
 // Main
 // ----
-
-// Verify arguments
-verifyArguments()
-
-// Do stuff according ot the arguments
-if (process.argv[2] === 'new') bookmakerNew();
+if (process.argv.length < 3)          printUsage();
+else if (process.argv[2] === 'new')   bookmakerNew();
 else if (process.argv[2] === 'build') bookmakerBuild();
+else                                  printUsage();
 
 
 // Functions
 // ---------
 function verifyArguments() {
-	if (process.argv.length < 3	) {
+	if (process.argv.length < 3) {
 		printUsage()
 	}
 }
 
 function printUsage() {
 	console.log(
-`
-${makeCyan('bookmaker init')}
+`${makeCyan('bookmaker init')}
     Initializes a new project.
 
 ${makeCyan('bookmaker build')}
     It builds the book.
+
+${makeCyan('bookmaker serve')}
+    It builds and serves continuosly.
 `);
 }
 
